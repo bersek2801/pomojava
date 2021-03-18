eb91facb3023c6e892dd9ba2d405b902af07d062
bersek2801 <47820824+bersek2801@users.noreply.github.com>
2021-03-18 16:03:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
 package pomojava
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+
+public class Main<INDEFINITE> extends Application
+{
+public static void main(String[] args)
+{
+        Application.launch(args);
+}
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        stage.setTitle("Pomodoro");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
+
+    }
+
+
